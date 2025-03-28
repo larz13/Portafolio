e5a37612b610f9613ca0d7c97ee3df2c53b8a3f1
Larz <l.a.r.d-1416@hotmail.com>
2025-03-28 13:41:41 -0500

    Agregados archivos estáticos 2

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -123,8 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'projects/static')]
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Ruta donde se guardarán los archivos estáticos
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'portafolio/static'),  # Ruta donde están tus archivos estáticos originales
+# ]
 
 
 # Default primary key field type
